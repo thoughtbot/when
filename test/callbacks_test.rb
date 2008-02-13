69ca9a78e08c97a03d7ba74677857c1c545797f0
dcroak <dcroak@7bbfaf0e-4d1d-0410-9690-a8bb5f8ef2aa>
2008-02-13 06:50:00 +0000

    added after_initialize callback tests
    git-svn-id: https://svn.thoughtbot.com/plugins/when/trunk@319 7bbfaf0e-4d1d-0410-9690-a8bb5f8ef2aa

--- test/callbacks_test.rb
+++ test/callbacks_test.rb
@@ -111,7 +111,7 @@ class CallbacksTest < Test::Unit::TestCase
         assert_equal 'new name', company.name unless company.frozen?
       end
     end
-    
+  
     define_method "test_after_find_with_if_condition_which_returns_true_should_change_company_name" do
       Company.send(:after_find, :change_name, { :if => condition })
       company = Company.new :name => 'thoughtbot', :callback_flag => true
@@ -141,6 +141,32 @@ class CallbacksTest < Test::Unit::TestCase
       company = Company.new :name => 'thoughtbot', :callback_flag => false
       assert company.save
       company = Company.find company.id
+      assert_equal 'new name', company.name
+    end
+  
+    define_method "test_after_initialize_with_if_condition_which_returns_true_should_change_company_name" do
+      Company.send(:after_initialize, :change_name, { :if => condition })
+      company = Company.new :name => 'thoughtbot', :callback_flag => true
+      assert company.save
+      company = Company.find company.id
+      assert_equal 'new name', company.name
+    end
+
+    define_method "test_after_initialize_with_if_condition_which_returns_false_should_not_change_company_name" do
+      Company.send(:after_initialize, :change_name, { :if => condition })
+      company = Company.new :name => 'thoughtbot', :callback_flag => false
+      assert_equal 'thoughtbot', company.name
+    end
+
+    define_method "test_after_initialize_with_unless_condition_which_returns_true_should_not_change_company_name" do
+      Company.send(:after_initialize, :change_name, { :unless => condition })
+      company = Company.new :name => 'thoughtbot', :callback_flag => true
+      assert_equal 'thoughtbot', company.name
+    end
+
+    define_method "test_after_initialize_with_unless_condition_which_returns_false_should_change_company_name" do
+      Company.send(:after_initialize, :change_name, { :unless => condition })
+      company = Company.new :name => 'thoughtbot', :callback_flag => false
       assert_equal 'new name', company.name
     end
   end
